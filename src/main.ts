9289af3e6b993a43e78bdf1cc89cbae67a102602
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-08-22 12:34:27 -0300

    Adicionado o Quasar Framework

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { Quasar } from 'quasar'
 import { createApp } from 'vue'
 import App from './App.vue'
+import quasarUserOptions from './quasar-user-options'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).use(store).use(router).mount('#app')
